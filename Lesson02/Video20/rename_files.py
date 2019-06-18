6e74852087f1864e4362040f2222c457f3221ebb
CP1984 <chingping1984@gmail.com>
2019-06-18 23:23:29 +0200

    Lesson 2, Video 20, Opening a File.

---
+++ Lesson02/Video20/rename_files.py
@@ -0,0 +1,10 @@
+import os
+
+def rename_files():
+    # (1) get file names form a folder
+    file_list = os.listdir(r"D:\github\UDACITY--Programming_Foundations_with_Python\Lesson02\Video20\prank")
+    print(file_list)
+    
+    # (2) for each file, rename filename
+    
+rename_files()
