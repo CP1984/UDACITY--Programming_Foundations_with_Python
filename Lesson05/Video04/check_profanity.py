984431ec04b07b732d09ae4dc4c6ea1471a04be8
CP1984 <chingping1984@gmail.com>
2019-07-04 23:28:42 +0200

    Lesson 5, Video 10, Accessing a Website with Code.

--- Lesson05/Video04/check_profanity.py
+++ Lesson05/Video04/check_profanity.py
@@ -1,7 +1,16 @@
+import urllib
+
 def read_text():
     quotes = open("D:\github\UDACITY--Programming_Foundations_with_Python\Lesson05\Video04\movie_quotes.txt")
     contents_of_file = quotes.read()
     print(contents_of_file)
     quotes.close()
+    check_profanity(contents_of_file)
     
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close()    
+
 read_text()
